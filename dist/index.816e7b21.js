fa2660ca991d2e9138d0702988ad439ca10b7a28
osman31 <osman.moh31@gmail.com>
2022-02-27 20:02:04 +0100

    project html css from first commit

---
+++ dist/index.816e7b21.js
@@ -0,0 +1,11 @@
+let navlink = document.getElementById("navlLink");
+function closemenu() {
+    navlink.style.display = "none";
+}
+function openmenu() {
+    navlink.style.display = "block";
+    navlink.style.right = "0px";
+    navlink.style.zIndex = "2";
+}
+
+//# sourceMappingURL=index.816e7b21.js.map
---
+++ src/app.js
@@ -0,0 +1,12 @@
+let navlink = document.getElementById("navlLink");
+
+function closemenu(){
+    navlink.style.display = "none";
+
+}
+
+function openmenu(){
+    navlink.style.display = "block";
+    navlink.style.right = "0px";
+    navlink.style.zIndex = "2";
+}
